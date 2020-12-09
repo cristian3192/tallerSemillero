1b404bb0af4852a8cd0e28173287c0be2d68e22e
Cristian <cris_t_15@hotmail.com>
2020-12-09 09:43:53 -0500

    Versión Inicial

---
+++ src/main/java/com/clearminds/ccc/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.ccc.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
